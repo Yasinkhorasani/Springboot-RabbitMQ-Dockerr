67f91e1eebed9d3a2e3fd05a4f61e2a24b228cb2
Yasin khorasani <yasinkhorasani@yahoo.com>
2023-01-22 19:33:10 +0100

    Create RabbitMQ JSON Producer/ Produce and send JSON Message

---
+++ src/main/java/springboot/rabbitmq/demo/publicher/RabbitMQJsonProducer.java
@@ -0,0 +1,28 @@
+package springboot.rabbitmq.demo.publicher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import springboot.rabbitmq.demo.dto.User;
+
+@Service
+public class RabbitMQJsonProducer {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.Json.key}")
+    private String routingJsonKey;
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonProducer.class);
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQJsonProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendJsonMessage(User user){
+        LOGGER.info(String.format("Json message sent -> %s",user.toString()));
+        rabbitTemplate.convertAndSend(exchange,routingJsonKey,user);
+    }
+}
